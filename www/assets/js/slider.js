664df04418ec1d9fe225979976046679a8c805e9
Janith Silva <janithravindu@gmail.com>
2019-09-15 00:57:07 +0530

    change slider speed

--- www/assets/js/slider.js
+++ www/assets/js/slider.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 2700,
         arrows: false,
         dots: false,
         pauseOnHover: true,
